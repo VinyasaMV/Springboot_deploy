4b69d7dd622c5d3eb0a43c7898c2890175b209d3
Vinyasa <vinyasa.mv@gmail.com>
2022-12-25 22:26:31 +0530

    adding mysql provisioning scrip file

---
+++ mysql.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+DATABASE_PASS='root'
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum install git zip unzip -y
+sudo yum install mariadb-server -y
+
+
+# starting & enabling mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+cd /tmp/
+git clone -b sql-query https://github.com/VinyasaMV/Springboot_deploy.git
+#restore the dump file for the application
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+sudo mysql -u root -p"$DATABASE_PASS" -e "create database employee_directory"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on employee_directory.* TO 'springstudent'@'localhost' identified by 'springstudent'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on employee_directory.* TO 'springstudent'@'%' identified by 'springstudent'"
+sudo mysql -u root -p"$DATABASE_PASS" employee_directory < /tmp/Springboot_deploy/employee.sql
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+# Restart mariadb-server
+sudo systemctl restart mariadb
+
+
+#starting the firewall and allowing the mariadb to access from port no. 3306
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart mariadb
