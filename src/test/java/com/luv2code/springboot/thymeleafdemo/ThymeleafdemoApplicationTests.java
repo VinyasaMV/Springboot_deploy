00d33dc9f00b955450784f4aad97ba6dafe2ed31
Vinyasa <vinyasa.mv@gmail.com>
2022-12-25 15:23:58 +0530

    Modified to generate war as build format

--- src/test/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
+++ src/test/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -9,9 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ThymeleafdemoApplicationTests {
 
-	@Test
+	/*@Test
 	public void contextLoads() {
 	}
-
+*/
 }
 
